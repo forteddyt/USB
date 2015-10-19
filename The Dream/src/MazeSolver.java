be1c622b5f00cef3dce49e67ec0014598a78f058
forteddyt <forteddyt@gmail.com>
2015-10-18 20:06:41 -0400

    Began Dead-End maze solver method

---
+++ The Dream/src/MazeSolver.java
@@ -0,0 +1,24 @@
+
+public class MazeSolver {
+	/*
+	 * "B" should be not possible paths "P" should be possible paths
+	 */
+
+	private String[][] maze;
+
+	public MazeSolver(String[][] maze) {
+		this.maze = maze;
+	}
+
+	public void deadEndStart() {
+
+	}
+
+	public void deadEndFiller(int r, int c) {
+		int counter = 0;
+	}
+
+	public String[][] getMaze() {
+		return maze;
+	}
+}
--- The Dream/src/SwingedMaze.java
+++ The Dream/src/SwingedMaze.java
@@ -7,39 +7,49 @@ import javax.swing.JPanel;
 
 public class SwingedMaze {
 	private JFrame frame = new JFrame();
+	private JPanel top = new JPanel();
+	/* ##################################### */
+	private RandomMaze myMaze = new RandomMaze(50, 50);
+	/* ##################################### */
 
 	public SwingedMaze() {
-
-		/* ##################################### */
-		RandomMaze myMaze = new RandomMaze(50, 50);
-		/* ##################################### */
-
 		frame.setTitle("Maze: " + myMaze.numCols() + " x " + myMaze.numRows());
 
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setSize(1000, 1000);
-
-		JPanel top = new JPanel();
 		top.setLayout(new GridLayout(myMaze.numRows(), myMaze.numCols()));
 		top.setBackground(Color.WHITE);
+
+		drawMaze();
+
+		top.setVisible(true);
+		frame.add(top);
+		frame.setVisible(true);
+	}
+
+	public void drawMaze() {
 		String[][] m = myMaze.getMaze();
 		for (int r = 0; r < myMaze.numRows(); r++) {
 			for (int c = 0; c < myMaze.numCols(); c++) {
 				JPanel l = new JPanel();
 				if (m[r][c].equals("X")) {
 					l.setBackground(Color.BLACK);
-				} else {
+				} else if (m[r][c].equals(" ")) {
 					l.setBackground(Color.WHITE);
+				} else if (m[r][c].equals("W")) {
+					l.setBackground(Color.RED);
+				} else if (m[r][c].equals("P")) {
+					l.setBackground(Color.GREEN);
 				}
 				l.setVisible(true);
 				top.add(l);
 			}
 		}
+	}
 
-		top.setVisible(true);
-		frame.add(top);
-		frame.setVisible(true);
+	public RandomMaze getRMaze() {
+		return myMaze;
 	}
 
 	public static void main(String[] args) {
