950dcd6267882641a9c4dc274e9352a66e3d1d89
forteddyt <forteddyt@gmail.com>
2015-11-15 09:57:11 -0500

    Eclipse wasn't displaying workstation. Re-added everything

---
+++ The Dream/src/CSGOWild/WinCalculator.java
@@ -0,0 +1,22 @@
+package CSGOWild;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class WinCalculator {
+	private LinkedHashSet games = new LinkedHashSet<String>();
+	private Scanner scan;
+
+	public WinCalculator() {
+
+	}
+
+	public void calcPercentage() {
+		String w = new String();
+	}
+
+	public static void main(String[] args) {
+		WinCalculator wcalc = new WinCalculator();
+		wcalc.calcPercentage();
+	}
+}
